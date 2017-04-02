04877a796becfb6998691afa42171a85a31c4248
rdobson94 <r_dobson@live.com>
2017-04-02 21:45:37 +0000

    Returning URLs in list

--- image-getter.py
+++ image-getter.py
@@ -1,8 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
 import urlparse
+img_links=[]
 
 url = "https://www.walmart.com/ip/54649026"
+
 result = requests.get(url)
 soup = BeautifulSoup(result.text, "html.parser")
 
@@ -10,17 +12,22 @@ soup = BeautifulSoup(result.text, "html.parser")
 og_image = (soup.find('meta', property='og:image') or
                     soup.find('meta', attrs={'name': 'og:image'}))
 if og_image and og_image['content']:
-    print og_image['content']
-    print ''
+    img_links.append(og_image['content'])
+    #print ''
 
 # This will look for a link tag with a rel attribute set to 'image_src'
 thumbnail_spec = soup.find('link', rel='image_src')
 if thumbnail_spec and thumbnail_spec['href']:
-    print thumbnail_spec['href']
-    print ''
+    img_links.append( thumbnail_spec ['href'])
+    #print ''
 
 
-image = """<img src="%s"><br />"""
+#image = """<img src="%s"><br />"""
+image = """"%s"""
 for img in soup.findAll("img", src=True):
-   print image % urlparse.urljoin(url, img["src"])
-   print ''
+    img_links.append(image % urlparse.urljoin(url, img["src"]))
+    
+print map(str,img_links)
+   
+    
+  
